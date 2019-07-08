032aece897f418c40668c9b27509a434fbe6b374
Andy Grzywacz <andy.grzywacz@Andys-MacBook-Pro.local>
2019-07-08 13:53:18 -0600

    svm module mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,6 +19,23 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import svm
+import numpy
+
+clf = svm.SVC(kernel="rbf", C = 10000)
+
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+unique, counts = numpy.unique(pred, return_counts=True)
+
+
+print (dict(zip(unique, counts)))
+
+# print clf.score(features_test, labels_test)
 
 
 
